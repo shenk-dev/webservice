68dd7a86063049be36028a090bff62ee784d8b00
shenk <shenk@tfinfo.cn>
2020-05-21 17:44:02 +0800

    update code

--- src/main/java/com/shenk/ws/cxf/CXFWebServicePublish.java
+++ src/main/java/com/shenk/ws/cxf/CXFWebServicePublish.java
@@ -7,6 +7,9 @@ import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 public class CXFWebServicePublish {
 
     public static void main(String[] args) {
+        /**
+         * 与jdk提供的ws不同,CXF发布ws时接口必须使用@WebService注解
+         */
         //定义WebService的发布地址，这个地址就是提供给外界访问Webervice的URL地址，URL地址格式为：http://ip:端口号/xxxx
         //String address = "http://192.168.152.1:8989/";这个WebService发布地址的写法是合法的
         //String address = "http://192.168.152.1:8989/Webservice";这个WebService发布地址的是合法的
